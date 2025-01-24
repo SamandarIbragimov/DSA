7482eb0e601f3883e68e4e867ca07b1bf394777a
Samandar Ibragimov <samandar0406@gmail.com>
2025-01-24 21:52:37 +0100

    Longest Common Prefix

---
+++ easy/LongestCommonPrefixClass.cs
@@ -0,0 +1,27 @@
+namespace leetcode.easy
+{
+    public class LongestCommonPrefixClass
+    {
+        public string LongestCommonPrefix(string[] strs) 
+        {
+            Array.Sort(strs);
+
+            var str1 = strs[0];
+            var str2 = strs[strs.Length - 1];
+            int i=0;
+
+            while(i < str1.Length)
+            {
+                if(str1[i] == str2[i])
+                {
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return str1.Substring(0, i);
+        }
+    }
+}
