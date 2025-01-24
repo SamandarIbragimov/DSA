76155c4912b27017bb78c749a2f5d6a10b4fb247
Samandar Ibragimov <samandar0406@gmail.com>
2025-01-24 05:30:36 +0100

    solved Palindrome Number

---
+++ easy/PalindromeNumber.cs
@@ -0,0 +1,21 @@
+namespace leetcode.easy
+{
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int x) 
+        {
+            if(x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+            int half = 0;
+            while (x > half) 
+            {
+                half = half * 10 + x % 10;
+                x = x / 10; 
+            }
+
+            return x == half || x == half / 10;
+        }
+    }
+}
