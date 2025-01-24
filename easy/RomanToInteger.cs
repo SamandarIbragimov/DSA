947615225c6eb056137cd9a6c0db929a823697ee
Samandar Ibragimov <samandar0406@gmail.com>
2025-01-24 06:17:39 +0100

    Roman to Integer

---
+++ easy/RomanToInteger.cs
@@ -0,0 +1,33 @@
+namespace leetcode.easy
+{
+    public class RomanToInteger
+    {
+        public int RomanToInt(string s) 
+        {
+            var dict = new Dictionary<char, int>()
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
+            };
+            int result = 0, current = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                current = dict[s[i]];
+                if(i < s.Length - 1 && current < dict[s[i+1]])
+                {
+                    result -= dict[s[i]];
+                }
+                else
+                {
+                    result += dict[s[i]];
+                }
+            }
+            return result;
+        }
+    }
+}
