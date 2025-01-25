2e8ed89de72c198963f4dca76c8c388bb681de60
Samandar Ibragimov <samandar0406@gmail.com>
2025-01-25 21:19:11 +0100

    Valid Parenthese - easy - solved

---
+++ easy/ValidParentheses.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+
+namespace leetcode.easy
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s) 
+        {
+            if(s.Length % 2 == 1)
+            {
+                return false;
+            }
+            Stack<char> stack = new Stack<char>();  
+            foreach (char c in s)  
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else if(stack.Count > 0)
+                {
+                    if((c == ')' && (char)stack.Peek() == '(') ||
+                        (c == ']' && (char)stack.Peek() == '[') ||
+                        (c == '}' && (char)stack.Peek() == '{'))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return stack.Count == 0? true: false;
+        }
+    }
+}
