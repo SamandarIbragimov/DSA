16cd1a252310cc713eaf11b9e20dcc59f01d40b9
Samandar Ibragimov <samandar0406@gmail.com>
2025-01-26 03:32:59 +0100

    Plus One - solved

---
+++ easy/PlusOneClass.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leetcode.easy {
+    public class PlusOneClass {
+        public int[] PlusOne(int[] digits) {
+            if (digits[digits.Length - 1] == 9) {
+                digits[digits.Length - 1] = 0;
+            } else {
+                digits[digits.Length - 1]++;
+                return digits;
+            }
+
+            for (int i = digits.Length - 2; i >= 0; i--) {
+                if (digits[i] == 9) {
+                    digits[i] = 0;
+                } else {
+                    digits[i]++;
+                    return digits;
+                }
+            }
+            var result = new int[digits.Length + 1]; result[0] = 1;
+            return result;
+        }
+    }
+}
