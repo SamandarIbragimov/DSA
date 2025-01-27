22a9668cebc05653c6615529b25d3a62c25af7b1
Samandar Ibragimov <samandar0406@gmail.com>
2025-01-27 21:07:50 +0100

    calling traversal function from helper

--- easy/BinaryTreeInorderTraversal.cs
+++ easy/BinaryTreeInorderTraversal.cs
@@ -4,16 +4,8 @@ namespace leetcode.easy {
     public class BinaryTreeInorderTraversal {
         public IList<int> InorderTraversal(TreeNode root) {
             List<int> list = new List<int>();
-            InorderTraversalHelper(root, list);
+            TreeTraversal.InOrderTraversalHelper(root, list);
             return list;
-        }
-
-        private void InorderTraversalHelper(TreeNode node, List<int> list) {
-            if (node != null) {
-                InorderTraversalHelper(node.left, list);
-                list.Add(node.val);
-                InorderTraversalHelper(node.right, list);
-            }
         }
     }
 }
