f64dd80708e0e8e50371f35bce8cb8e898f3011b
Samandar Ibragimov <samandar0406@gmail.com>
2025-01-29 21:36:20 +0100

    Power of Two - solved

---
+++ easy/PowerOfTwo.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leetcode.easy
+{
+    public class PowerOfTwo
+    {
+        public bool IsPowerOfTwo(int n) {
+            if(n <= 0){
+                return false;
+            }
+            while(n != 1){
+                if(n % 2 !=0){
+                    return false;
+                } else {
+                    n = n / 2;
+                }
+            }
+            return true;
+        }
+    }
+}
