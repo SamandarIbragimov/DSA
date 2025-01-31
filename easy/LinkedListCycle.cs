9603d8ca43ce540a1a32ad99db1211e1fc093b6b
Samandar Ibragimov <samandar0406@gmail.com>
2025-01-31 22:07:46 +0100

    Linked List Cycle - solved

---
+++ easy/LinkedListCycle.cs
@@ -0,0 +1,17 @@
+using leetcode.helper;
+
+namespace leetcode.easy {
+    public class LinkedListCycle {
+        public bool HasCycle(ListNode head) {
+            Dictionary<ListNode, bool> dict = new Dictionary<ListNode, bool>();
+            while(head != null) {
+                if(dict.ContainsKey(head)){
+                    return true;
+                }
+                dict.Add(head,true);
+                head = head.next;
+            }
+            return false;
+        }
+    }
+}
