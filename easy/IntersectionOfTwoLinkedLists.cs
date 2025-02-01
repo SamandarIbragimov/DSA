7e4a19bd1d681ec80fd60f363634bfa99307220c
Samandar Ibragimov <samandar0406@gmail.com>
2025-02-01 21:03:03 +0100

    name space style fixed

--- easy/IntersectionOfTwoLinkedLists.cs
+++ easy/IntersectionOfTwoLinkedLists.cs
@@ -1,20 +1,21 @@
 using leetcode.helper;
 
-namespace leetcode.easy;
-public class IntersectionOfTwoLinkedLists {
-    public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
-        Dictionary<ListNode,bool> visited = [];
-        while(headA != null) {
-            visited.Add(headA,true);
-            headA = headA.next;
-        }
-        while(headB != null){
-            if(visited.ContainsKey(headB)){
-                return headB;
-            } else{
-                headB = headB.next;
+namespace leetcode.easy {
+    public class IntersectionOfTwoLinkedLists {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+            Dictionary<ListNode,bool> visited = [];
+            while(headA != null) {
+                visited.Add(headA,true);
+                headA = headA.next;
             }
+            while(headB != null){
+                if(visited.ContainsKey(headB)){
+                    return headB;
+                } else {
+                    headB = headB.next;
+                }
+            }
+            return null;
         }
-        return null;
     }
 }
