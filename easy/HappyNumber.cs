e19d597b8ff7c9b305df3052f0fea27a5afbfe10
Samandar Ibragimov <samandar0406@gmail.com>
2025-02-01 21:29:11 +0100

    Happy Number - solved

---
+++ easy/HappyNumber.cs
@@ -0,0 +1,26 @@
+namespace leetcode.easy {
+    public class HappyNumber {
+        public bool IsHappy(int n) {
+            Dictionary<int,bool> dic = [];
+            while (n > 1) {
+                if (dic.ContainsKey(n)) {
+                    return false;
+                }
+                dic.Add(n, true);
+                n = Sum(n);
+            }
+            return true;
+        }
+
+        private int Sum(int n){
+            var s = n.ToString();
+            var sum = 0;
+            int digit;
+            foreach(var c in s){
+                digit = c - 48;
+                sum += digit * digit;
+            }
+            return sum;
+        }
+    }
+}
