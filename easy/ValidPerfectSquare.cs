a362402274d84e5d87083769063d674dcecb5fee
Samandar Ibragimov <samandar0406@gmail.com>
2025-02-02 21:25:37 +0100

    Valid Perfect Square - solved

---
+++ easy/ValidPerfectSquare.cs
@@ -0,0 +1,20 @@
+namespace leetcode.easy {
+    public class ValidPerfectSquare {
+        public bool IsPerfectSquare(int num) {
+            long left = 1;
+            long right = num;
+
+            while (left <= right) {
+                long mid = (left + right) / 2;
+                if(mid * mid > num) {
+                    right = mid - 1;
+                } else if(mid * mid < num){
+                    left = mid + 1;
+                } else {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
