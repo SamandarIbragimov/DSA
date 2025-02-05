3fc270cfc3cdcb33bccd2ed65af2f4ce9e8b669a
Samandar Ibragimov <samandar0406@gmail.com>
2025-02-05 22:43:49 +0100

    Perfect Number - solved

---
+++ easy/PerfectNumber.cs
@@ -0,0 +1,22 @@
+namespace leetcode.easy {
+    public class PerfectNumber {
+        public bool CheckPerfectNumber(int num) {
+            int sum = 0;
+            for(int i = 1;i*i < num;i++){
+                if(num % i == 0){
+                    if(i != (num / i)){
+                        sum += i;
+                        sum += (num/i);
+                    } else {
+                        sum += i;
+                    }
+                }
+            }
+            if((sum-num) == num){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
