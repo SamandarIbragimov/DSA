3debf0a99e8addca63d21c68200726efa01dadbc
Samandar Ibragimov <samandar0406@gmail.com>
2025-02-09 20:43:44 +0100

    New Helper Class created

---
+++ helper/BitManipulation.cs
@@ -0,0 +1,12 @@
+namespace leetcode.helper {
+    public static class BitManipulation {
+        public static int CountBit(int n){
+            int count = 0;
+            while(n > 0){
+                count++;
+                n &= n - 1;
+            }
+            return count;
+        }
+    }
+}
