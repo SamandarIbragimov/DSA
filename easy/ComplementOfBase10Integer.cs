3193d635b0ac8d7af6bce0b99f67241313f5378b
Samandar Ibragimov <samandar0406@gmail.com>
2025-02-15 12:08:15 +0100

    Complement of Base 10 Integer - solved

---
+++ easy/ComplementOfBase10Integer.cs
@@ -0,0 +1,14 @@
+namespace leetcode.easy {
+    public class ComplementOfBase10Integer {
+        public int BitwiseComplement(int n) {
+            if(n == 0) {
+                return 1;
+            }
+            int k = 1;
+            while(k <= n) {
+                k = k << 1;
+            }
+            return (k-1)^n;
+        }
+    }
+}
