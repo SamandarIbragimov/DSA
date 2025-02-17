b55ae9a7564e0532979678214d79ab12f2f13a1c
Samandar Ibragimov <samandar0406@gmail.com>
2025-02-17 20:23:07 +0100

    modified

--- medium/JumpGameII.cs
+++ medium/JumpGameII.cs
@@ -1,12 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace leetcode.medium
-{
-    public class JumpGameII
-    {
-        
+namespace leetcode.medium {
+    public class JumpGameII {
+        public int Jump(int[] nums) {
+            int res = 0, l = 0, r = 0, end;
+            while(r < nums.Length - 1) {
+                end = 0;
+                for(int i = l; i <= r; i++) {
+                    end = i + nums[i] > end ? i + nums[i] : end;
+                } 
+                l = r + 1;
+                r = end;
+                res +=1;
+            }
+            return res;
+        }
     }
 }
