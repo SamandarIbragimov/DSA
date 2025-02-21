cf8c373d0966c363ea8e43bbcd0ce17864e5af69
Samandar Ibragimov <samandar0406@gmail.com>
2025-02-21 15:39:59 +0100

    code style

--- helper/BitManipulation.cs
+++ helper/BitManipulation.cs
@@ -1,11 +1,14 @@
 namespace leetcode.helper {
     public static class BitManipulation {
-        public static int CountBit(int n){
+        public static int CountBit(int n) {
+
             int count = 0;
+
             while(n > 0){
                 count++;
                 n &= n - 1;
             }
+            
             return count;
         }
     }
