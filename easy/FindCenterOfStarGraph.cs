dc3fad95d4785094c763d58df6dd99db6c6874a1
Samandar Ibragimov <samandar0406@gmail.com>
2025-02-23 11:19:15 +0100

    Find Center of Star Graph - solved

---
+++ easy/FindCenterOfStarGraph.cs
@@ -0,0 +1,7 @@
+namespace leetcode.easy {
+    public class FindCenterOfStarGraph {
+        public int FindCenter(int[][] edges) {
+            return (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])  ? edges[0][0] : edges[0][1];
+        }
+    }
+}
